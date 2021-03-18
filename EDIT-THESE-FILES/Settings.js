2e81ae5e395e181ad9512383522642c91b7edadb
Isaac Wasserman <isaacrwasserman@gmail.com>
2021-03-18 11:13:19 -0400

    fixed ellipses

--- EDIT-THESE-FILES/Settings.js
+++ EDIT-THESE-FILES/Settings.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b1d6d8202769593b5ad3560730ae083cc69d664d40982dd7d2d869b7f60ecd2
-size 26247
+oid sha256:d1b170f3c561edfc9b188289cfe77eda036ff1b87e14855974c333295257e844
+size 26224
